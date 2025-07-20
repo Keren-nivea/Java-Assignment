c3c14c72a933aba291eb4eb0e2ac0a29bbdec52b
Keren-nivea <153603635+Keren-nivea@users.noreply.github.com>
2025-07-20 21:35:20 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Hello {
-    public static void main(String[] args) {
-        Random r=new Random();
-        boolean x=r.nextBoolean();
-        System.out.println(x);
-    }    
-}
